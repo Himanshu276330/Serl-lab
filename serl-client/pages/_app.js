ee61b90a0c30ea6337d49a24b45b71335e79c4e4
Shivani <shivani14306@gmail.com>
2023-03-20 19:04:35 +0530

    login page done

--- serl-client/pages/_app.js
+++ serl-client/pages/_app.js
@@ -1,6 +1,7 @@
 import '... @/styles/globals.css'
 import '... @/styles/navbar.css'
 import '... @/styles/signup.css'
+import '... @/styles/login.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
